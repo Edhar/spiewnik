6534d25a2903429a2833aeccc4ac228b3521150a
Edhar <e.zahdai@bylectric.com>
2013-04-08 11:40:26 +0300

    double back button handler commented

--- src/com/zelwise/spiewnik/MainActivity.java
+++ src/com/zelwise/spiewnik/MainActivity.java
@@ -229,6 +229,19 @@ public class MainActivity  extends Activity implements OnClickListener {
     FocusedFirstSongsListViewItem();
   }
   
+  /*
+  private static final long DOUBLE_PRESS_INTERVAL =  some value in ns.;
+  private long lastPressTime;
+
+  @Override
+  public void onBackPressed() {
+      long pressTime = System.nanoTime();
+      if(pressTime - lastPressTime <= DOUBLE_PRESS_INTERVAL) {
+          // this is a double click event
+      }
+      lastPressTime = pressTime;
+  }*/
+  
   @Override
   public void onBackPressed() {
 	  
