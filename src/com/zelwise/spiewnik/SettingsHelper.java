5ab816d6c37f1335245d2f6656f2b92c7cd711f0
Edhar <e.zahdai@bylectric.com>
2013-04-08 11:48:32 +0300

    SettingsHelper.java

---
+++ src/com/zelwise/spiewnik/SettingsHelper.java
@@ -0,0 +1,9 @@
+package com.zelwise.spiewnik;
+
+public class SettingsHelper {
+	
+	public SettingsHelper(){
+		
+	}
+	// see here http://twigstechtips.blogspot.com/2010/11/android-save-and-load-settings.html
+}
