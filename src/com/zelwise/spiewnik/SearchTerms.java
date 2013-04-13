529c3b63838ce29ad386e05ee28d078d62637ee5
Edhar <zelwise@gmail.com>
2013-04-13 15:20:53 +0300

    use grobal settings for list

--- src/com/zelwise/spiewnik/SearchTerms.java
+++ src/com/zelwise/spiewnik/SearchTerms.java
@@ -1,54 +1,54 @@
 package com.zelwise.spiewnik;
 
 public class SearchTerms {
-	
-	private SearchBy searchBy = SearchBy.Text;
-	public SearchBy SearchBy(){
-		return searchBy;
+	private SettingsHelper settings;
+
+	public Integer SongsPerPage() {
+		return settings.SongPerPage();
 	};
-	
-	private String searchText = "";
-	public String SearchText(){
-		return searchText;
+
+	public Boolean SeachByAndShowSongNumbersInResult() {
+		return settings.SeachByAndShowSongNumbersInResult();
 	};
-	
-	private Integer songsPerPage = SettingsHelper.DefaultValues.SongPerPage;
-	public Integer SongsPerPage(){
-		return songsPerPage;
-	};
-	
+
 	private String orderByString = "";
-	public String OrderByString(){
+
+	public String OrderByString() {
 		return orderByString;
 	};
-	
-	private Boolean seachByAndShowSongNumbersInResult = SettingsHelper.DefaultValues.SeachByAndShowSongNumbersInResult;
-	public Boolean SeachByAndShowSongNumbersInResult(){
-		return seachByAndShowSongNumbersInResult;
+
+	private SearchBy searchBy = SearchBy.Text;
+
+	public SearchBy SearchBy() {
+		return searchBy;
 	};
-	
+
+	private String searchText = "";
+
+	public String SearchText() {
+		return searchText;
+	};
+
 	private int currentPage = 1;
-	public int CurrentPage(){
+
+	public int CurrentPage() {
 		return currentPage;
 	};
-	public void CurrentPage(Integer newValue){
-		currentPage=newValue;
+
+	public void CurrentPage(Integer newValue) {
+		currentPage = newValue;
 	};
-	
-	public SearchTerms(SettingsHelper settings,SearchBy searchBy,String searchText,String orderByString){
-		this(searchBy,searchText, settings.SongPerPage(), 1,orderByString,settings.SeachByAndShowSongNumbersInResult());
-	}
-	
-	public SearchTerms(SettingsHelper settings,SearchBy searchBy,String searchText,int currentPage,String orderByString){
-		this(searchBy,searchText, settings.SongPerPage(), currentPage,orderByString,settings.SeachByAndShowSongNumbersInResult());
+
+	public SearchTerms(SettingsHelper settings, SearchBy searchBy,
+			String searchText, String orderByString) {
+		this(settings, searchBy, searchText, 1, orderByString);
 	}
 
-	public SearchTerms(SearchBy searchBy,String searchText, Integer songsPerPage,Integer currentPage,String orderByString,Boolean seachByAndShowSongNumbersInResult){
+	public SearchTerms(SettingsHelper settings, SearchBy searchBy,
+			String searchText, Integer currentPage, String orderByString) {
 		this.searchBy = searchBy;
 		this.searchText = searchText.trim();
-		this.songsPerPage = songsPerPage;
 		this.orderByString = orderByString;
-		this.seachByAndShowSongNumbersInResult = seachByAndShowSongNumbersInResult;
 		this.currentPage = currentPage;
 	}
 }
